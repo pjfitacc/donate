b15fd4dbf8827678f880632c68c5a45e92d4edb2
pjfitacc <pjfitacc@gmail.com>
2025-03-19 14:35:32 -0400

    ReccurringInput

--- my-app/src/components/checkout-grid/forms/payment-form/index.js
+++ my-app/src/components/checkout-grid/forms/payment-form/index.js
@@ -1,23 +1,10 @@
 import * as React from "react";
-// @ts-ignore
-import Alert from "@mui/material/Alert";
-import Box from "@mui/material/Box";
 
-import CardActionArea from "@mui/material/CardActionArea";
-import CardContent from "@mui/material/CardContent";
-import Checkbox from "@mui/material/Checkbox";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormLabel from "@mui/material/FormLabel";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import RadioGroup from "@mui/material/RadioGroup";
 import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
 
-import { CardGiftcard, Repeat } from "@mui/icons-material";
 import PaymentSchedulePicker from "./PaymentSchedulePicker";
 import CreditCardInput from "./CreditCardInput";
+import RecurringInput from "./RecurringInput";
 
 export default function PaymentForm() {
   const [paymentSchedule, setPaymentSchedule] = React.useState("oneTime");
@@ -59,48 +46,7 @@ export default function PaymentForm() {
         handlePaymentScheduleChange={handlePaymentScheduleChange}
         setPaymentSchedule={setPaymentSchedule}
       />
-      {paymentSchedule === "recurring" && (
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Typography variant="subtitle1" sx={{ fontWeight: "medium" }}>
-            Create a Recurring Donation
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            W.I.P. RECURRING FUNCTIONALITY IS STILL A WORK IN PROGRESS.
-          </Typography>
-          <Box sx={{ display: "flex", gap: 1 }}>
-            <Typography variant="body1" sx={{ color: "text.secondary" }}>
-              Default Options:
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: "medium" }}>
-              Monthly, Quarterly, Yearly
-            </Typography>
-          </Box>
-          <Box sx={{ display: "flex", gap: 1 }}>
-            <Typography variant="body1" sx={{ color: "text.secondary" }}>
-              Payment Start Date:
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: "medium" }}>
-              May 4th 2025 (info ?)
-            </Typography>
-          </Box>
-          <Box sx={{ display: "flex", gap: 1 }}>
-            <Typography variant="body1" sx={{ color: "text.secondary" }}>
-              Frequency:
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: "medium" }}>
-              Weekly
-            </Typography>
-          </Box>
-          <Box sx={{ display: "flex", gap: 1 }}>
-            <Typography variant="body1" sx={{ color: "text.secondary" }}>
-              End of Recurring Cycle:
-            </Typography>
-            <Typography variant="body1" sx={{ fontWeight: "medium" }}>
-              June 2025
-            </Typography>
-          </Box>
-        </Box>
-      )}
+      {paymentSchedule === "recurring" && <RecurringInput />}
       <CreditCardInput
         cardNumber={cardNumber}
         handleCardNumberChange={handleCardNumberChange}
