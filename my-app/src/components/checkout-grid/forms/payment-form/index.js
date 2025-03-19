91c49fb5955282f76501afc3e50cc5a655173ef0
pjfitacc <pjfitacc@gmail.com>
2025-03-19 14:22:56 -0400

    Payment Schedule Picker Radio Group
    inside payment-form

--- my-app/src/components/checkout-grid/forms/payment-form/index.js
+++ my-app/src/components/checkout-grid/forms/payment-form/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 // @ts-ignore
 import Alert from "@mui/material/Alert";
 import Box from "@mui/material/Box";
-import MuiCard from "@mui/material/Card";
+
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import Checkbox from "@mui/material/Checkbox";
@@ -16,43 +16,7 @@ import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
 import SimCardRoundedIcon from "@mui/icons-material/SimCardRounded";
 import { CardGiftcard, Repeat } from "@mui/icons-material";
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  border: "1px solid",
-  // @ts-ignore
-  borderColor: (theme.vars || theme).palette.divider,
-  width: "100%",
-  "&:hover": {
-    background:
-      "linear-gradient(to bottom right, hsla(210, 100%, 97%, 0.5) 25%, hsla(210, 100%, 90%, 0.3) 100%)",
-    borderColor: "primary.light",
-    boxShadow: "0px 2px 8px hsla(0, 0%, 0%, 0.1)",
-    ...theme.applyStyles("dark", {
-      background:
-        "linear-gradient(to right bottom, hsla(210, 100%, 12%, 0.2) 25%, hsla(210, 100%, 16%, 0.2) 100%)",
-      borderColor: "primary.dark",
-      boxShadow: "0px 1px 8px hsla(210, 100%, 25%, 0.5) ",
-    }),
-  },
-  [theme.breakpoints.up("md")]: {
-    flexGrow: 1,
-    maxWidth: `calc(50% - ${theme.spacing(1)})`,
-  },
-  variants: [
-    {
-      // @ts-ignore
-      props: ({ selected }) => selected,
-      style: {
-        // @ts-ignore
-        borderColor: (theme.vars || theme).palette.primary.light,
-        ...theme.applyStyles("dark", {
-          // @ts-ignore
-          borderColor: (theme.vars || theme).palette.primary.dark,
-        }),
-      },
-    },
-  ],
-}));
+import PaymentSchedulePicker from "./PaymentSchedulePicker";
 
 const PaymentContainer = styled("div")(({ theme }) => ({
   display: "flex",
@@ -121,97 +85,11 @@ export default function PaymentForm() {
 
   return (
     <Stack spacing={{ xs: 3, sm: 6 }} useFlexGap>
-      <FormControl component="fieldset" fullWidth>
-        <FormLabel htmlFor="">Donation Schedule</FormLabel>
-        <RadioGroup
-          aria-label="Payment options"
-          name="paymentSchedule"
-          value={paymentSchedule}
-          onChange={handlePaymentScheduleChange}
-          sx={{
-            display: "flex",
-            flexDirection: { xs: "column", sm: "row" },
-            gap: 2,
-          }}
-        >
-          <Card
-            // @ts-ignore
-            selected={paymentSchedule === "oneTime"}
-          >
-            <CardActionArea
-              onClick={() => setPaymentSchedule("oneTime")}
-              sx={{
-                ".MuiCardActionArea-focusHighlight": {
-                  backgroundColor: "transparent",
-                },
-                "&:focus-visible": {
-                  backgroundColor: "action.hover",
-                },
-              }}
-            >
-              <CardContent
-                sx={{ display: "flex", alignItems: "center", gap: 1 }}
-              >
-                <CardGiftcard
-                  fontSize="small"
-                  sx={[
-                    (theme) => ({
-                      color: "grey.400",
-                      ...theme.applyStyles("dark", {
-                        color: "grey.600",
-                      }),
-                    }),
-                    paymentSchedule === "oneTime" && {
-                      color: "primary.main",
-                    },
-                  ]}
-                />
-                <Typography sx={{ fontWeight: "medium" }}>
-                  One Time Donation
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card
-            // @ts-ignore
-            selected={paymentSchedule === "recurring"}
-          >
-            <CardActionArea
-              onClick={() => setPaymentSchedule("recurring")}
-              sx={{
-                ".MuiCardActionArea-focusHighlight": {
-                  backgroundColor: "transparent",
-                },
-                "&:focus-visible": {
-                  backgroundColor: "action.hover",
-                },
-              }}
-            >
-              <CardContent
-                sx={{ display: "flex", alignItems: "center", gap: 1 }}
-              >
-                <Repeat
-                  fontSize="small"
-                  sx={[
-                    (theme) => ({
-                      color: "grey.400",
-                      ...theme.applyStyles("dark", {
-                        color: "grey.600",
-                      }),
-                    }),
-                    paymentSchedule === "recurring" && {
-                      color: "primary.main",
-                    },
-                  ]}
-                />
-                <Typography sx={{ fontWeight: "medium" }}>
-                  Recurring Donation
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </RadioGroup>
-      </FormControl>
+      <PaymentSchedulePicker
+        paymentSchedule={paymentSchedule}
+        handlePaymentScheduleChange={handlePaymentScheduleChange}
+        setPaymentSchedule={setPaymentSchedule}
+      />
       {paymentSchedule === "recurring" && (
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
           <Typography variant="subtitle1" sx={{ fontWeight: "medium" }}>
