16d88403a24a4d6d55f679f83f1114c1567431b0
pjfitacc <pjfitacc@gmail.com>
2025-03-24 15:20:47 -0400

    Custom Success Message if No Preference Donation Chosen

--- my-app/src/components/PaymentSuccess.js
+++ my-app/src/components/PaymentSuccess.js
@@ -8,13 +8,14 @@ function PaymentSuccess({ response }) {
     const qgwTransaction = response['quantumGatewayTransactionResponse']
     const submittedForm = useFormStore.getState();
     const transactionID = qgwTransaction[TransQGWdbeApprovedResponseSequence.TRANSACTION_ID]
+    const noPreference = submittedForm.beneficiary.includes("No preference")
 
     return (
         <Stack id="complete-donation" spacing={2} useFlexGap>
             <Typography variant="h1">🎁</Typography>
             <Typography variant="h5">Thank you for your donation, {submittedForm.firstName}!</Typography>
             <Typography variant="body1" sx={{ color: "text.secondary" }}>
-                Your support for <strong>{submittedForm.beneficiary}</strong> will go a long way.
+                Your support {!noPreference && <>for <strong>{submittedForm.beneficiary}</strong></>} will go a long way.
                 <br></br>
                 This donation's number is <strong>#{transactionID}</strong>.
                 <br></br>
