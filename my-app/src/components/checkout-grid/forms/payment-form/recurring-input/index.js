fbb087d7922713ec7bbefc9a77ffb7ca5ac2dbda
pjfitacc <pjfitacc@gmail.com>
2025-03-27 18:44:26 -0400

    Recurring Input Form Fields
    Included so far:
    1. initialIntervalAmount (number)
    2. recurAmount (number)
    3. overrideRecurPrice
    
    overrideRecurPrice is set to "Y". Indicating that the recurAmount will not be the default recipe ID, but it will be recurAmount from now on.
    
    initialIntervalAmount is null in its initial state so its first value when rendered in the RecurringInput form is the regular amount field in formStore. Otherwise, it'll take the initialIntervalAmount from then on.
    
    Other required recurring input form fields:
    1. recipeID - select component - use the RecurringRecipes in models.js
    2. timesToRecur - number
    3. overrideRecurDay - "Y"/"N" if the donor wants to override the default RecurringRecipes cycleDay to today.

--- my-app/src/components/checkout-grid/forms/payment-form/recurring-input/index.js
+++ my-app/src/components/checkout-grid/forms/payment-form/recurring-input/index.js
@@ -1,16 +1,84 @@
-import { Typography } from "@mui/material";
+import {
+  FormHelperText,
+  FormLabel,
+  OutlinedInput,
+  styled,
+  Typography,
+} from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
 import React from "react";
+import useFormStore from "stores/formStore";
+import useErrorStore from "stores/errorStore";
+
+const FormGrid = styled(Grid)(() => ({
+  display: "flex",
+  flexDirection: "column",
+}));
 
 function RecurringInput() {
+  const setField = useFormStore((state) => state.setField);
+  setField("overrideRecurPrice", "Y");
+
+  const recurAmount = useFormStore((state) => state.recurAmount);
+  const initialIntervalAmount = useFormStore(
+    (state) => state.initialIntervalAmount
+  );
+  const amount = useFormStore((state) => state.amount);
+
+  const errors = useErrorStore((state) => state);
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
       <Typography variant="subtitle1" sx={{ fontWeight: "medium" }}>
-        Create a Recurring Donation
+        Donation Plan Setup
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        <b>W.I.P. RECURRING FUNCTIONALITY IS STILL A WORK IN PROGRESS.</b>
-      </Typography>
+      <Grid container spacing={3}>
+        <FormGrid size={{ xs: 12, md: 6 }}>
+          <FormLabel htmlFor="initialIntervalAmount" required>
+            Custom Interval 1 Price
+          </FormLabel>
+          <OutlinedInput
+            id="initialIntervalAmount"
+            name="initialIntervalAmount"
+            value={
+              initialIntervalAmount === null ? amount : initialIntervalAmount
+            }
+            onChange={(e) => setField("initialIntervalAmount", e.target.value)}
+            type="number"
+            autoComplete="number"
+            required
+            size="small"
+            error={!!errors.initialIntervalAmount}
+            style={{ borderColor: errors.initialIntervalAmount ? "red" : "" }}
+          />
+          {!!errors.initialIntervalAmount && (
+            <FormHelperText error id="initialIntervalAmountError">
+              {errors.initialIntervalAmount}
+            </FormHelperText>
+          )}
+        </FormGrid>
+        <FormGrid size={{ xs: 12, md: 6 }}>
+          <FormLabel htmlFor="recurAmount" required>
+            Price per Interval
+          </FormLabel>
+          <OutlinedInput
+            id="recurAmount"
+            name="recurAmount"
+            value={recurAmount}
+            onChange={(e) => setField("recurAmount", e.target.value)}
+            type="number"
+            required
+            size="small"
+            style={{ borderColor: errors.recurAmount ? "red" : "" }}
+          />
+          {!!errors.recurAmount && (
+            <FormHelperText error id="recurAmountError">
+              {errors.recurAmount}
+            </FormHelperText>
+          )}
+        </FormGrid>
+      </Grid>
       <Box sx={{ display: "flex", gap: 1 }}>
         <Typography variant="body1" sx={{ color: "text.secondary" }}>
           Default Options:
