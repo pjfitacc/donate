817b8d2eb9c7b55043150214e82d667d4a7bb2a0
pjfitacc <pjfitacc@gmail.com>
2025-04-01 18:54:37 -0400

    InitialPaymentSelect override CSS

--- my-app/src/pages/InitialPaymentSelect.js
+++ my-app/src/pages/InitialPaymentSelect.js
@@ -9,6 +9,7 @@ import { styled, useColorScheme } from "@mui/material/styles";
 import PHJLogo from "components/logos/PHJLogo";
 import GradientBackground from "components/background/GradientBackground";
 import { AccountBalance, CreditCard } from "@mui/icons-material";
+import ColorModeSelect from "shared-theme/ColorModeSelect";
 const Card = styled(MuiCard)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
@@ -20,11 +21,17 @@ const Card = styled(MuiCard)(({ theme }) => ({
   [theme.breakpoints.up("sm")]: {
     maxWidth: "600px",
   },
+  backgroundColor: "rgba(5, 7, 10, 0.4)",
+  borderBottomColor: "rgba(51, 60, 77, 0.6)",
+  borderLeftColor: "rgba(51, 60, 77, 0.6)",
+  borderRightColor: "rgba(51, 60, 77, 0.6)",
+  borderTopColor: "rgba(51, 60, 77, 0.6)",
   boxShadow:
-    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+    "rgba(9, 11, 17, 0.5) 0px 5px 15px 0px, rgba(19, 23, 32, 0.08) 0px 15px 35px -5px",
+  color: "rgb(255, 255, 255)",
   ...theme.applyStyles("dark", {
     boxShadow:
-      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+      "rgba(9, 11, 17, 0.5) 0px 5px 15px 0px, rgba(19, 23, 32, 0.08) 0px 15px 35px -5px",
   }),
 }));
 
@@ -52,25 +59,61 @@ const Container = styled(Stack)(({ theme }) => ({
   },
 }));
 
+// const BlackButton = styled(Button)(({ theme }) => ({
+//   backgroundColor: "rgb(11, 14, 20)",
+//   borderBottomColor: "rgb(51, 60, 77)",
+//   borderLeftColor: "rgb(51, 60, 77)",
+//   borderRightColor: "rgb(51, 60, 77)",
+//   borderTopColor: "rgb(51, 60, 77)",
+//   color: "rgb(255, 255, 255)",
+//   "&:hover": {
+//     backgroundColor: "rgb(30, 35, 45)", // Darker shade on hover
+//     // You can also change border colors on hover if needed
+//     borderBottomColor: "rgb(70, 80, 100)",
+//     borderLeftColor: "rgb(70, 80, 100)",
+//     borderRightColor: "rgb(70, 80, 100)",
+//     borderTopColor: "rgb(70, 80, 100)",
+//   },
+// }));
+
+const BlackButton = styled(Button)(({ theme }) => ({
+  backgroundColor: "rgb(11, 14, 20)",
+  borderBottomColor: "rgb(51, 60, 77)",
+  borderLeftColor: "rgb(51, 60, 77)",
+  borderRightColor: "rgb(51, 60, 77)",
+  borderTopColor: "rgb(51, 60, 77)",
+  color: "white", // Ensure text is visible over gradients
+  position: "relative",
+  overflow: "hidden", // Ensures gradients don't spill outside borders
+  zIndex: 1, // Needed for pseudo-elements
+
+  "&::before": {
+    content: '""',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: `
+      radial-gradient(at 9% 57%, hsla(45,72%,65%,1) 0px, transparent 100%),
+      radial-gradient(at 56% 40%, hsla(21,91%,62%,1) 0px, transparent 100%),
+      radial-gradient(at 18% 75%, hsla(15,77%,69%,1) 0px, transparent 100%),
+      radial-gradient(at 31% 75%, hsla(5,97%,60%,1) 0px, transparent 100%),
+      radial-gradient(at 75% 39%, hsla(356,66%,63%,1) 0px, transparent 100%)
+    `,
+    opacity: 0,
+    zIndex: -1,
+  },
+
+  "&:hover": {
+    color: "black", // Dark text for better contrast with bright gradients
+    "&::before": {
+      opacity: 1, // Reveal gradients on hover
+    },
+    border: "1px solid black !important",
+  },
+}));
 function InitialPaymentSelectContainer({ setPaymentType }) {
-  const { mode, setMode } = useColorScheme();
-
-  React.useEffect(() => {
-    setMode("dark");
-    // Cleanup function (runs on unmount)
-    return () => {
-      setMode("light");
-    };
-  }, [setMode]);
-
-  if (!mode) {
-    return null;
-  }
-
-  if (mode === "light") {
-    return null;
-  }
-
   return (
     <Container direction="column" justifyContent="space-between">
       {/* <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} /> */}
@@ -89,7 +132,7 @@ function InitialPaymentSelectContainer({ setPaymentType }) {
         </Typography>
         <Divider>Choose Payment Type</Divider>
         <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
-          <Button
+          <BlackButton
             fullWidth
             onClick={() => {
               setPaymentType("creditCard");
@@ -98,8 +141,8 @@ function InitialPaymentSelectContainer({ setPaymentType }) {
             startIcon={<CreditCard />}
           >
             Credit Card
-          </Button>
-          <Button
+          </BlackButton>
+          <BlackButton
             fullWidth
             onClick={() => {
               setPaymentType("bank");
@@ -108,7 +151,7 @@ function InitialPaymentSelectContainer({ setPaymentType }) {
             startIcon={<AccountBalance />}
           >
             Bank
-          </Button>
+          </BlackButton>
           {/* <Typography sx={{ textAlign: "center" }}>
               Don&apos;t have an account?{" "}
               <Link
