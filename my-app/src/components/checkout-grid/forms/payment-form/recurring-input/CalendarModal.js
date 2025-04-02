a6169cd49f8382725741dc541316c2902abc6523
pjfitacc <pjfitacc@gmail.com>
2025-04-02 12:09:32 -0400

    Recurring Calculations Documentation
    Goes over the essential inputs like RID, recurAmount, etc. to determine the date outputs that the Calendar Component consumes and shows.

--- my-app/src/components/checkout-grid/forms/payment-form/recurring-input/CalendarModal.js
+++ my-app/src/components/checkout-grid/forms/payment-form/recurring-input/CalendarModal.js
@@ -12,6 +12,27 @@ const style = {
   justifyContent: "center",
 };
 
+// Required Recurring Items to display:
+
+// 1. RID (formStore field = recipeID)
+// Sets the recurring frequency cycle
+// IMPORTANT: the RID recipe will depend on the quantum gateway account that you use and what type of recurring frequency you mapped to a particular RID.
+// ex: weekly, monthly, standard yearly. etc.
+
+// 2. Recur times (formStore field = timesToRecur)
+// Example: You charge $50.00 on first transaction and now want to charge them $50.00
+// per month for a 11 months. Then the recur_times would be 11. After that, the recurring
+// automatically stops for that entry
+
+// 3. Recur Amount (formStore field = recurAmount)
+//   example: The amount each point on the Recurring Cycle is
+
+// 4. Initial Interval Amount (formStore field = initialIntervalAmount)
+// The initial amount you pay TODAY on submit.
+// If the initial amount date and the next recurring date with recur amount overlap, the initial amount takes precedence over recur amount.
+
+// For further information on the Recurring Fields: https://www.quantumgateway.com/files/recurring_help.pdf
+
 function CalendarModal({ open, handleClose }) {
   return (
     <Modal
@@ -32,6 +53,26 @@ function CalendarModal({ open, handleClose }) {
       </LocalizationProvider>
     </Modal>
   );
+}
+
+function calculateRecurringAmountsAndDates(
+  recipeID,
+  timesToRecur,
+  recurAmount,
+  initialIntervalAmount
+) {
+  // recipeID determines:
+  // 1. the time length between each recurring interval
+  // - ex: weekly
+  // 2. the beginning cycle date and subsequent cycles.
+  // - ex: every Tuesday
+  //
+  // timesToRecur determines when we end the recurring interval
+  // - ex: how long the list of our recurring dates will be.
+  //
+  // recurAmount determines how much money USD each cycle will be.
+  //
+  // InitialIntervalAmount will show USD price on time of submission (today)
 }
 
 export default CalendarModal;
